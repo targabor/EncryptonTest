7a6165f61051d6636f3f2515b719420116e509b9
targabor <tar.gabor14@gmail.com>
2020-08-10 12:59:02 +0200

    Made a PBKDF2 encoder

--- src/Encrypt.java
+++ src/Encrypt.java
@@ -1,7 +1,11 @@
+import javax.crypto.SecretKeyFactory;
+import javax.crypto.spec.PBEKeySpec;
 import java.nio.charset.StandardCharsets;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 import java.security.SecureRandom;
+import java.security.spec.InvalidKeySpecException;
+import java.security.spec.KeySpec;
 import java.util.Arrays;
 
 public class Encrypt {
@@ -9,7 +13,7 @@ public class Encrypt {
     private static final SecureRandom random = new SecureRandom();
     private static  MessageDigest md = null;
 
-
+    //Generate the salt
     private static byte[] generateSalt(){
 
         byte[] salt = new byte[16];
@@ -17,10 +21,11 @@ public class Encrypt {
         return salt;
     }
 
+    //Generate SHA-512 hash
     public static String hashSha512(String userPassword){
 
         byte[] hashedPassword = null;
-        byte[] salt = "Bimbi".getBytes();
+        byte[] salt = generateSalt();
         try {
             md = MessageDigest.getInstance("SHA-512");
             md.update(salt);
@@ -33,6 +38,37 @@ public class Encrypt {
             }
         }
         return "Hiba";
+    }
+
+    //Generate PBKDF2 hash
+    protected final static int ITERATION_COUNT = 10000;
+
+    public static String hashPBKDF2(String userPassword){
+
+        byte[] hash = null;
+        byte[] salt = generateSalt();
+
+        KeySpec spec = new PBEKeySpec(userPassword.toCharArray(),salt,ITERATION_COUNT,128);
+        SecretKeyFactory keyFactory = null;
+
+        try{
+            keyFactory = SecretKeyFactory.getInstance("PBKDF2WithHmacSHA512");
+        }catch (NoSuchAlgorithmException nsaEx){
+            System.out.println("There is a NoSuchAlgorithmException at hashPBKDF2:\n"+nsaEx);
+        }
+
+        if(keyFactory!= null) {
+            try {
+                hash = keyFactory.generateSecret(spec).getEncoded();
+            } catch (InvalidKeySpecException iksEx) {
+                System.out.println("There is a InvalidKeySpecException at hashPBKDF2:\n"+iksEx);
+            }
+        }
+
+        if(hash != null)
+            return Arrays.toString(salt) + ":" + Arrays.toString(hash);
+        else
+            return "Hiba";
     }
 
 }
--- src/Main.java
+++ src/Main.java
@@ -10,5 +10,7 @@ public class Main {
         String password = userInput.nextLine();
         System.out.println("SHA-512");
         System.out.println(Encrypt.hashSha512(password));
+        System.out.println("PBKDF2");
+        System.out.println(Encrypt.hashPBKDF2(password));
     }
 }
